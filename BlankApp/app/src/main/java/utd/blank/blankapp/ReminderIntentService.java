16bdcd49813ad3de2e74ceb867f8eeb2ad30449a
Ridge Frederick <ridge.frederick@gmail.com>
2015-11-30 19:35:59 -0600

    Rewriting notification handler

--- BlankApp/app/src/main/java/utd/blank/blankapp/ReminderIntentService.java
+++ BlankApp/app/src/main/java/utd/blank/blankapp/ReminderIntentService.java
@@ -1,10 +1,12 @@
 package utd.blank.blankapp;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.Context;
+import android.service.notification.StatusBarNotification;
 import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsManager;
 import android.util.Log;
@@ -26,13 +28,7 @@ public class ReminderIntentService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         Log.d(TAG, "onHandleIntent called");
         NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder nBuilder =
-                new NotificationCompat.Builder(this)
-                        .setContentTitle("My notification")
-                        .setContentText("Hello World!");
         Intent resultIntent = new Intent(this, MainActivity.class);
-        // Because clicking the notification opens a new ("special") activity, there's
-        // no need to create an artificial back stack.
         PendingIntent resultPendingIntent =
                 PendingIntent.getActivity(
                         this,
@@ -40,11 +36,24 @@ public class ReminderIntentService extends IntentService {
                         resultIntent,
                         PendingIntent.FLAG_UPDATE_CURRENT
                 );
-        nBuilder.setContentIntent(resultPendingIntent);
-        notificationManager.notify(NOTIFICATION_ID, nBuilder.build());
 
-        SmsManager smsManager = SmsManager.getDefault();
-        Log.d(TAG, "sending sms");
-        smsManager.sendTextMessage("+15406415112", null, "Reminder not answered", null, null);
+        Notification n = new NotificationCompat.Builder(this)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!")
+                        .setContentIntent(resultPendingIntent)
+                        .setSmallIcon(R.drawable.notify).build();
+
+        // Because clicking the notification opens a new ("special") activity, there's
+        // no need to create an artificial back stack.
+        notificationManager.notify(NOTIFICATION_ID, n);
+
+        StatusBarNotification[] notifications = notificationManager.getActiveNotifications();
+        for(StatusBarNotification s : notifications){
+            if(s.equals(n)){
+                SmsManager smsManager = SmsManager.getDefault();
+                Log.d(TAG, "sending sms");
+                smsManager.sendTextMessage("+15406415112", null, "Reminder not answered", null, null);
+            }
+        }
     }
 }
